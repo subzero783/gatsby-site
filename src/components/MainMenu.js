9d581a772555c57922d149a4e5cfb0ab4e587a81
Gustavo <gusta1296@gmail.com>
2021-09-25 10:15:36 -0700

    left at video 16

---
+++ src/components/MainMenu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {graphql, StaticQuery} from 'gatsby'
+
+const MainMenu = () => (
+    <div>
+        MainMenu component
+    </div>
+)
+
+export default MainMenu
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,46 +6,15 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import MainMenu from './MainMenu'
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header" 
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+const Layout = ({ children }) => (
+  <div>
+    <MainMenu />
+    {children}
+  </div>
+)
 
 export default Layout
